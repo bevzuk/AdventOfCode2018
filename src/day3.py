8796d67e3842fd51d8a9867c2a2945b06623ffbc
Anton Bevzuk <a.bevzuk@dodopizza.com>
2018-12-04 09:23:50 +0300

    Solution (still wrong) :(

--- src/day3.py
+++ src/day3.py
@@ -1,3 +1,6 @@
+import array
+
+
 class Piece:
     def __init__(self, s):
         split = s.split(' ')
@@ -26,7 +29,62 @@ class Piece:
     def height(self):
         return self._height
 
-class FabricSolver:
-    def Foo(self):
-        print("Hello");
-        return 0;
+class Fabric:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+        self._area = []
+        for row in range(0, height):
+            self._area.append([])
+            for column in range(0, width):
+                self._area[row].append(0)
+
+    def Claim(self, piece):
+        for row in range(0, piece.height):
+            for column in range(0, piece.width):
+                self._area[piece.top_offset + row][piece.left_offset + column] += 1
+
+    def Area(self):
+        area = "\n"
+        for row in range(0, self._height):
+            for column in range(0, self._width):
+                if self._area[row][column] == 0:
+                    area += "."
+                else:
+                    area += str(self._area[row][column])
+            area += '\n'
+        return area
+
+    @property
+    def OverlappingInches(self):
+        overlappingInches = 0
+        for row in range(0, self._height):
+            for column in range(0, self._width):
+                if self._area[row][column] > 1:
+                    overlappingInches += 1
+        return overlappingInches
+
+
+text_file = open("day3.input.txt", "r")
+pieces = list(map(lambda x: Piece(x.replace("\n", "")), text_file.readlines()))
+
+max_width = 0
+max_height = 0
+for piece in pieces:
+    if piece.left_offset + piece.width > max_width:
+        max_width = piece.left_offset + piece.width
+    if piece.top_offset + piece.height > max_height:
+        max_height = piece.top_offset + piece.height
+
+fabric = Fabric(max_width, max_height)
+for piece in pieces:
+    fabric.Claim(piece)
+
+print(fabric._width)
+print(fabric._height)
+print(fabric.OverlappingInches)
+
+out_file = open("day3.out.txt", "w")
+out_file.write(fabric.Area())
+out_file.close()
+
--- src/day3_ut.py
+++ src/day3_ut.py
@@ -1,10 +1,55 @@
 import unittest
-from src.day3 import Piece, FabricSolver
+from parameterized import parameterized
+from src.day3 import Piece, Fabric
 
 class WhenCreatePiece(unittest.TestCase):
     def test_that_it_populates_attributes(self):
-        piece = Piece("#1 @ 906,735: 28x17");
+        piece = Piece("#1 @ 906,735: 28x17")
         self.assertEqual(906, piece.top_offset)
-        self.assertEqual(735, piece.left_offset);
-        self.assertEqual(28, piece.width);
-        self.assertEqual(17, piece.height);
+        self.assertEqual(735, piece.left_offset)
+        self.assertEqual(28, piece.width)
+        self.assertEqual(17, piece.height)
+
+class WhenClaimPiece(unittest.TestCase):
+    def test_that_fabric_counts_single_piece_1x1(self):
+        piece = Piece("#1 @ 0,0: 1x1")
+        fabric = Fabric(2, 2)
+        fabric.Claim(piece)
+
+        self.assertEqual("\n"
+                         "1.\n"
+                         "..\n", fabric.Area())
+
+    def test_that_fabric_counts_single_piece_2x2(self):
+        piece = Piece("#1 @ 1,1: 2x2")
+        fabric = Fabric(4, 4)
+        fabric.Claim(piece)
+
+        self.assertEqual("\n"
+                         "....\n"
+                         ".11.\n"
+                         ".11.\n"
+                         "....\n", fabric.Area())
+
+    def test_that_fabric_counts_overlapping_pieces_2x2(self):
+        fabric = Fabric(4, 4)
+        fabric.Claim(Piece("#1 @ 0,0: 2x2"))
+        fabric.Claim(Piece("#1 @ 1,1: 2x2"))
+
+        self.assertEqual("\n"
+                         "11..\n"
+                         "121.\n"
+                         ".11.\n"
+                         "....\n", fabric.Area())
+
+    @parameterized.expand([
+        ["#1 @ 0,0: 2x2", "#1 @ 1,1: 2x2", 1],
+        ["#2 @ 0,0: 2x2", "#2 @ 0,0: 2x2", 4],
+    ])
+    def test_that_fabric_counts_overlapping_inches_count(self, piece1, piece2, overlappingInches):
+        fabric = Fabric(4, 4)
+        fabric.Claim(Piece(piece1))
+        fabric.Claim(Piece(piece2))
+
+        self.assertEqual(overlappingInches, fabric.OverlappingInches)
+
